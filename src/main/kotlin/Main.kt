5fd6722206e7594bbeaf2f3582f986e39c9feda8
Arshad Ahmad <itsarshadahmad@gmail.com>
2022-12-09 11:05:29 +0530

    recursion bug fixed, new learning concepts added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -109,6 +109,9 @@ fun main(args: Array<String>) {
     testObject.buck() // class method/function call
     testObject.str = "Shawn" // Accessing setter
     println(testObject.str) // Accessing getter
+
+//    Object class -> Anonymous class or Design (Singleton) Pattern class
+    testingObjectKeywordClass() // Calling Anonymous class or Test Object keyword class
 }
 
 //    Access modifier -> by default variable is public, support all access modifiers
@@ -160,19 +163,42 @@ open class Test() {
         return a + 12
     }
 
-    // Getter & setter
-    var str: String
-        get() = str
+    // Getter & setter -> field refer to assigned value
+    var str: String = ""
+        get() = field.toString()
         set(value) {
-            str = value
+            field = value
+            field = value
         }
 }
 
 // inheritance
-class Test1 : Test() {
+// pass value to parent constructor
+class Test1(name: String) : Test(name), Task { // Task in interface, multiple interface can be inherited
     override fun buck() {
         println("Adam Page")
     }
+
+    //    Interface method implementation is required if inherited
+    override fun display() {
+        TODO("Not yet implemented")
+    }
+}
+
+// Abstract class
+abstract class Done {
+    //    Abstract function
+    abstract fun doIt()
+}
+
+// Interface
+interface Task {
+    //    default method -> it can have implementation inside interface
+    fun defaultMethodInterface() {
+        println("Default Method of interface")
+    }
+
+    fun display() // Abstract method of interface
 }
 
 // Tailrec -> used to calculate big number of recursion to avoid stackoverflow error.
@@ -184,3 +210,43 @@ tailrec fun fact(num: BigInteger, result: BigInteger) {
     }
 }
 
+// Data class -> It allow extra feature like toString, hashCode, equals, copy, compare, and many other features by default.
+// It adds getter, setter and make class ready to deal with data, and hold data.
+// It requires parameter to describe values, so it can generate all fields.
+data class Profile(val name: String, val age: Int, val profession: String)
+
+fun testDataClass() {
+    var pr = Profile("Tanmay", 30, "Comedian")
+}
+
+// Object keyword -> It can create anonymous (inner) class
+// It is also used to create design pattern like singleton class
+val West = object {
+    private var profiles = arrayListOf<Profile>()
+    fun showProfile() {
+        for (profile in profiles) {
+            println(profile.toString())
+        }
+    }
+}
+
+// Or
+object West1 {
+    private var profiles = arrayListOf<Profile>()
+    fun saveProfile(profile: Profile) {
+        profiles.add(profile)
+    }
+
+    fun showProfile() {
+        for (profile in profiles) {
+            println(profile.toString())
+        }
+    }
+}
+
+// You call anonymous or object class by its name like static classes
+fun testingObjectKeywordClass() {
+    var w = West.toString() // It acts as object or return type
+    var w1 = West1.saveProfile(Profile("John", 15, "Student"))
+    var w2 = West1.showProfile()
+}
